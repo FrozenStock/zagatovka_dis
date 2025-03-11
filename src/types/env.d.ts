16e374a791efe9fd58b6943e3ba613b8bca39339
medieage <skyline0084@gmail.com>
2025-03-11 23:50:55 +0300

    aol

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,9 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NEXT_PUBLIC_SUPABASE_URL: string;
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
-    SUPABASE_SERVICE_ROLE_KEY: string;
-    SUPABASE_JWT_SECRET: string;
+    SUPABASE_URL: string;
+    SUPABASE_ANON_KEY: string;
     POSTGRES_HOST: string;
     POSTGRES_USER: string;
     POSTGRES_PASSWORD: string;
